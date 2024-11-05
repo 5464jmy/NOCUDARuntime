7d5696569cfcbecd5899ac863dbb71bf43d1622f
5464jmy <2782324121@qq.com>
2024-11-05 11:01:17 +0800

    更新更多接口 有些未测试

--- src/person.cpp
+++
@@ -1,9 +0,0 @@
-#include "person.h"
-
-#include <utility>
-
-Person::Person(std::string  name, int age) : name(std::move(name)), age(age) {}
-
-std::string Person::introduce() const {
-    return "My name is " + name + " and I am " + std::to_string(age) + " years old.";
-}
